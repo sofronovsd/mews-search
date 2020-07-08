831143a062368359a2f719a1cf32ef162fd365e5
Sofronov Semyon <sofronov.semyon@gmail.com>
2020-07-08 13:15:03 +0400

    init: set up base environment

---
+++ src/components/Wrapper.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    width: 1024px;
+    height: 100vh;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+`;
+
+export default Wrapper;
